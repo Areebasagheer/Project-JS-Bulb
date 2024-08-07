0201349bb9af0eda1592a969ff3458bd5f1c7079
Areebasagheer <areebasagheer79@gmail.com>
2024-08-07 18:46:30 +0500

    project bulb

---
+++ add.js
@@ -0,0 +1,13 @@
+var img = document.getElementById("bulb")
+
+function on(){
+    img.src = "img/on-bulb.jpg"
+    img.style.width="600px"
+    img.style.height="350px"    
+}
+
+function off(){
+    img.src = "img/word-image7-removebg-preview.jpg"
+    img.style.width="590px"
+    img.style.height="360px"
+}
